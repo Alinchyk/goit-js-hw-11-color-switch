ea66e74fcb07afd932a54c77801e5fb4c50a42ca
Alinchyk <alina.mykhalchuk09@gmail.com>
2023-03-29 18:50:55 +0200

    project refactoring

--- src/index.js
+++ src/index.js
@@ -13,28 +13,25 @@ const colors = [
   "#E6E6FA",
 ];
 
+const changeColor = () => {
+  let hexColor = randomIntegerFromInterval(0, colors.length - 1);
+  document.body.style.background = colors[hexColor];
+};
+
+let intervalId;
+const startChange = () => {
+  startBtn.disabled = true;
+  intervalId = setInterval(changeColor, 2000);
+};
+
+const stopChange = () => {
+  clearInterval(intervalId);
+  startBtn.disabled = false;
+};
+
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-let intervalId;
-
-function changeColor() {
-  let hexColor = randomIntegerFromInterval(0, colors.length - 1);
-
-  document.body.style.background = colors[hexColor];
-}
-
-function startChange() {
-  startBtn.disabled = true;
-  intervalId = setInterval(changeColor, 2000);
-}
-
-function stopChange() {
-  clearInterval(intervalId);
-  startBtn.disabled = false;
-}
-
-// слухачі
 startBtn.addEventListener("click", startChange);
 stopBtn.addEventListener("click", stopChange);
